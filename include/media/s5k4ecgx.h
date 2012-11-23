bf7bf249bc2e4c71370f47e925ae49e0f44cbe5a
Sylwester Nawrocki <sylvester.nawrocki@gmail.com>
2012-11-23 22:44:08 +0100

    s5k4ecgx: Initial camera sensor driver from Insignal
    Basic working driver from Insignal. This camera sensor is tested on
    Origen board.
    
    Signed-off-by: Giridhar Maruthy <giridhar.maruthy@linaro.org>
    Signed-off-by: Tushar Behera <tushar.behera@linaro.org>

--- include/media/s5k4ecgx.h
+++ include/media/s5k4ecgx.h
@@ -1,8 +1,6 @@
-/*
- * S5K4ECGX image sensor header file
+/* include/media/s5k4ecgx.h
  *
- * Copyright (C) 2012, Linaro
- * Copyright (C) 2012, Samsung Electronics Co., Ltd.
+ * Copyright (C) 2010, SAMSUNG ELECTRONICS
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -10,28 +8,18 @@
  * (at your option) any later version.
  */
 
-#ifndef S5K4ECGX_H
-#define S5K4ECGX_H
-
-/**
- * struct s5k4ecgx_gpio - data structure describing a GPIO
- * @gpio : GPIO number
- * @level: indicates active state of the @gpio
- */
-struct s5k4ecgx_gpio {
-	int gpio;
-	int level;
-};
-
-/**
- * struct ss5k4ecgx_platform_data- s5k4ecgx driver platform data
- * @gpio_reset:	 GPIO driving RESET pin
- * @gpio_stby :	 GPIO driving STBY pin
- */
+#define S5K4ECGX_DRIVER_NAME	"S5K4ECGX"
 
 struct s5k4ecgx_platform_data {
-	struct s5k4ecgx_gpio gpio_reset;
-	struct s5k4ecgx_gpio gpio_stby;
+	unsigned int default_width;
+	unsigned int default_height;
+	unsigned int pixelformat;
+	int freq;	/* MCLK in Hz */
+	int (*set_power)(int enable);
+#ifdef CONFIG_VIDEO_S5K4ECGX_FLASHLIGHT
+	int (*flash_onoff)(int);
+	int (*af_assist_onoff)(int);
+	int (*torch_onoff)(int);
+#endif
 };
 
-#endif /* S5K4ECGX_H */
