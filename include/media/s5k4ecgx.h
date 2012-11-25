bf409ac71832d06f176dbc1a2c1200c48ec1426e
Sangwook Lee <sangwook.lee@plusgenie.com>
2012-11-25 11:09:09 +0000

    Revert "s5k4ecgx: Initial camera sensor driver from Insignal"
    This reverts commit bf7bf249bc2e4c71370f47e925ae49e0f44cbe5a.
    
    Conflicts:
    
    	drivers/media/i2c/s5k4ecgx/s5k4ecgx.c
    
    Signed-off-by: Sangwook Lee <sang.lee@plusgenie.com>

--- include/media/s5k4ecgx.h
+++ include/media/s5k4ecgx.h
@@ -1,6 +1,8 @@
-/* include/media/s5k4ecgx.h
+/*
+ * S5K4ECGX image sensor header file
  *
- * Copyright (C) 2010, SAMSUNG ELECTRONICS
+ * Copyright (C) 2012, Linaro
+ * Copyright (C) 2012, Samsung Electronics Co., Ltd.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -8,18 +10,28 @@
  * (at your option) any later version.
  */
 
-#define S5K4ECGX_DRIVER_NAME	"S5K4ECGX"
+#ifndef S5K4ECGX_H
+#define S5K4ECGX_H
 
-struct s5k4ecgx_platform_data {
-	unsigned int default_width;
-	unsigned int default_height;
-	unsigned int pixelformat;
-	int freq;	/* MCLK in Hz */
-	int (*set_power)(int enable);
-#ifdef CONFIG_VIDEO_S5K4ECGX_FLASHLIGHT
-	int (*flash_onoff)(int);
-	int (*af_assist_onoff)(int);
-	int (*torch_onoff)(int);
-#endif
+/**
+ * struct s5k4ecgx_gpio - data structure describing a GPIO
+ * @gpio : GPIO number
+ * @level: indicates active state of the @gpio
+ */
+struct s5k4ecgx_gpio {
+	int gpio;
+	int level;
 };
 
+/**
+ * struct ss5k4ecgx_platform_data- s5k4ecgx driver platform data
+ * @gpio_reset:	 GPIO driving RESET pin
+ * @gpio_stby :	 GPIO driving STBY pin
+ */
+
+struct s5k4ecgx_platform_data {
+	struct s5k4ecgx_gpio gpio_reset;
+	struct s5k4ecgx_gpio gpio_stby;
+};
+
+#endif /* S5K4ECGX_H */
